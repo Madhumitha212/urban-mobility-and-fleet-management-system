065ac2ec2f6e52b3a6533c46514fb9f992e451e9
Madhumitha212 <madhuravi324@gmail.com>
2025-12-25 15:26:51 +0530

    setup:[Madhu:] - created UC7: Prevent duplictes vehicles

--- source/eco_ride_main.py
+++ source/eco_ride_main.py
@@ -1,6 +1,37 @@
+from electric_car import ElectricCar
+from electric_scooter import ElectricScooter
+
 class EcoRideMain:
+    @staticmethod
     def main():
         print("Welcome to Eco-Ride Urban Mobility System")
+
+        fleet_hubs = {
+            "Downtown" : [],
+            "Airport" : []
+        }
+
+        car1 = ElectricCar("EV701", "Tesla Model 3", 90, 5)
+        car2 = ElectricCar("EV701", "Tesla Model Y", 85, 7)
+        scooter1 = ElectricScooter("EV702", "Ather 450X", 80, 85)
+
+        def add_vehicle(hub, vehicle):
+            for vehicles in fleet_hubs.values():
+                if vehicle in vehicles:
+                    print(f"Duplicate vehicle ID detected: {vehicle.vehicle_id}")
+                    return 
+            fleet_hubs[hub].append(vehicle)
+            print(f"Vehicle {vehicle.vehicle_id} added to {hub}")
+
+        add_vehicle("Downtown", car1)
+        add_vehicle("Airport", car2)
+        add_vehicle("Downtown", scooter1)
+
+        print("\nFinal Fleet status:")
+        for hub, vehicles in fleet_hubs.items():
+            print(f"\nHub: {hub}")
+            for v in vehicles:
+                v.display_details()
 
 
 if __name__ == "__main__":
