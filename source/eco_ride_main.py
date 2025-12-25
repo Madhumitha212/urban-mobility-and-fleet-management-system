1d2ad3275c7a00fc73e8481df5322f3151051b31
Madhumitha212 <madhuravi324@gmail.com>
2025-12-25 14:37:51 +0530

    setup:[Madhu:] - created Usecase 5

--- source/eco_ride_main.py
+++ source/eco_ride_main.py
@@ -1,6 +1,21 @@
+from electric_car import ElectricCar
+from electric_scooter import ElectricScooter
+
 class EcoRideMain:
+    @staticmethod
     def main():
         print("Welcome to Eco-Ride Urban Mobility System")
+
+        car = ElectricCar("EC401", "Tesla Model 3", 85, 7)
+        scooter = ElectricScooter("ES402", "Ather 450X", 70, 80)
+
+        vehicles = [car, scooter]
+
+        print("\nTrip Cost Calculation (Polymorphism):")
+
+        for vehicle in vehicles:
+            cost = vehicle.calculate_trip_cost(10)
+            print(f"{vehicle.model} -> Trip Cost for 10 km: {cost}")
 
 
 if __name__ == "__main__":
