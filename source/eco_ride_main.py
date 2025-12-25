9997c04756925603479cc6e4eb1b3b18691e98dc
Madhumitha212 <madhuravi324@gmail.com>
2025-12-25 20:28:15 +0530

    setuo:[Madhu:] - modified uc-12

--- source/eco_ride_main.py
+++ source/eco_ride_main.py
@@ -7,15 +7,18 @@ class EcoRideMain:
         print("Welcome to Eco-Ride Urban Mobility System")
 
         vehicles = [
-            ElectricCar("EV1201", "Tesla Model Y", 60, 7),
-            ElectricCar("EV1202", "Tesla Model 3", 60, 5),
+            ElectricCar("EV1201", "Tesla Model 3", 60, 5),
+            ElectricCar("EV1202", "Tesla Model Y", 90, 7),
             ElectricScooter("EV1203", "Ather 450X", 40, 85),
-            ElectricScooter("EV1204", "Ola S1", 40, 90)
+            ElectricScooter("EV1204", "Ola S1", 75, 90)
         ]
 
-        vehicles.sort(key=lambda v: (v._Vehicle__battery_percentage, v.model))
+        vehicles.sort(
+            key=lambda v: v._Vehicle__battery_percentage,
+            reverse=True
+        )
 
-        print("\nVehicles sorted by Battery then Model:")
+        print("\nVehicles sorted by Battery Percentage(Descending):")
         for v in vehicles:
             v.display_details()
 
