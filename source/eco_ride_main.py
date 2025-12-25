0ed2dce02c14b6df8e31ca897e2dfb0b00cc8eb9
Madhumitha212 <madhuravi324@gmail.com>
2025-12-25 13:07:29 +0530

    setup:[Madhu:] - implemented uc 4

--- source/eco_ride_main.py
+++ source/eco_ride_main.py
@@ -1,6 +1,16 @@
+from electric_car import ElectricCar
+from electric_scooter import ElectricScooter
+
 class EcoRideMain:
+    @staticmethod
     def main():
         print("Welcome to Eco-Ride Urban Mobility System")
+
+        car = ElectricCar("EC401", "Tesla Model X", 85, 7)
+        scooter = ElectricScooter("ES402", "Ather 450X", 70, 80)
+
+        print("\nCar Trip Cost (10km): ", car.calculate_trip_cost(10))
+        print("\nScooter Trip Cost (10km): ", scooter.calculate_trip_cost(10))
 
 
 if __name__ == "__main__":
---
+++ source/vehicle.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    def __init__(self, vehicle_id, model, battery_percentage):
+        self.vehicle_id = vehicle_id
+        self.model = model
+        self.__battery_percentage = None
+        self.set_battery_percentage(battery_percentage)
+
+    def get_battery_percentage(self):
+        return self.__bettery_percenatge
+    def set_battery_percentage(self, battery_percentage):
+        if 0 <= battery_percentage <= 100:
+            self.__battery_percentage = battery_percentage
+        else:
+            print("Invalid battery percentage! Must be between 0 and 100.")
+    def display_details(self):
+        print("Vehicle ID:",self.vehicle_id)
+        print("Model:",self.model)
+        print("Battery Percentage:",self.__battery_percentage, "%")
+
+    @abstractmethod
+    def calculate_trip_cost(self,distance):
+        pass
